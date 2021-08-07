543532f7132fd1c4d6634ffcfc70c57a9480dd44
SmirnovaT <75753895+SmirnovaT@users.noreply.github.com>
2021-08-07 17:55:57 +0300

    добавила удаление элемента из списка

--- Классы/Class_список3.py
+++ Классы/Class_список3.py
@@ -1,19 +1,23 @@
 class Todo:
     def __init__(self):
-        self.list = []
+        self.items = []
 
     def input_todo(self, task, percent):
         self.task = task
         self.percent = percent
         sum = f"{task}: {percent}"
-        return self.list.append(sum)
+        return self.items.append(sum)
 
-    def remove(self, b):
-        self.list.remove(b)
-
+    def remove(self, delete):
+        self.delete = delete
+        for el in self.items:
+            if delete in el:
+                self.items.remove(el)
+            else:
+                print("Данной задачи в списке нет")
 
     def dis(self):
-        return self.list
+        return self.items
 
 
 obj = Todo()
@@ -23,15 +27,22 @@ while True:
     if user == "показать":
         print("Список: ", obj.dis())
     elif user == "добавить":
-        task = input("Добавьте задачу в список: ")
-        percent = int(input("Процент выполнения задачи: "))
+        task = input("Добавьте задачу в список: ").lower()
+        while True:
+            try:
+                percent = int(input("Введите процент выполнения задачи: "))
+                break
+            except ValueError:
+                print("Пожалуйста, введите процент выполнения в цифрах")
         obj.input_todo(task, percent)
         print("Список: ", obj.dis())
     elif user == "удалить":
-        list = input("Что удалить из списка: ")
-        obj.remove(list)
+        delete = input("Что удалить из списка: ").lower()
+        obj.remove(delete)
         print("Список: ", obj.dis())
     elif user == "стоп":
         break
     else:
         print("Заданной команды нет, повторите ввод")
+
+
